a743bfa1e1a5d8bd8a59a55fe3f4b7f15582aad0
Nik <bakhamyt@gmail.com>
2023-10-23 20:06:01 +0300

    5

---
+++ number5.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+void strEdit() {
+
+    string text = "ajhfjksfhjskdhfjschchchchchc!chchch dffdsfd, ajdhdjka ch dfnsdjhj ";
+
+    string substring = "ch";
+
+
+    size_t punctuationPos = text.find_first_of(".!?,;:");
+
+    if (punctuationPos != string::npos) {
+
+        size_t substringPos = text.find(substring);
+
+        if (substringPos != string::npos && substringPos < punctuationPos) {
+
+            text.erase(substringPos, punctuationPos - substringPos);
+        }
+    }
+
+    cout << "editing text: " << text << endl;
+
+}
+
+
+int main() {
+    strEdit();
+    return 0;
+}
+
+
+
+
+
+
